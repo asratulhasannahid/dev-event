8828775dd80756889727adac36d454ff04c28e8c
asratulhasannahid <mdnahid.prg@gmail.com>
2026-02-09 22:04:12 +0600

    Dev Event

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h2 className="text-5xl text-blue-600">About AHN</h2>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/components/Hallo.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+const Hallo = () => {
+  console.log("I am client!");
+  return <div>Hallo Component</div>;
+};
+
+export default Hallo;
