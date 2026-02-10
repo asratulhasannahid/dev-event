eee24a740736f49de8d61f33c5fd20171fadec4d
asratulhasannahid <mdnahid.prg@gmail.com>
2026-02-10 19:40:59 +0600

    Nest Routing

---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h2 className="text-5xl">Analytics</h2>
+    </div>
+  );
+};
+
+export default page;
---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h2 className="text-5xl">Users</h2>
+    </div>
+  );
+};
+
+export default page;
