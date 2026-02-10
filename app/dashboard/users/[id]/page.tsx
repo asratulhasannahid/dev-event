e5f311890a6c989a803767be8b6924cfd4a503fe
asratulhasannahid <mdnahid.prg@gmail.com>
2026-02-10 19:52:36 +0600

    dynamic routing without specification

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+      <h2 className="text-3xl">UserDetails</h2>
+    </div>
+  );
+};
+
+export default page;
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 const page = () => {
   return (
     <div>
-      <h2 className="text-5xl">Users</h2>
+      <h2 className="text-5xl">Dashboard Users</h2>
+      <ul className="mt-10">
+        <li>
+          <Link href="/dashboard/users/1">User - 1</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/2">User - 2</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/3">User - 3</Link>
+        </li>
+        <li>
+          <Link href="/dashboard/users/4">User - 4</Link>
+        </li>
+      </ul>
     </div>
   );
 };
