fb6759ddb12621d63227e0af9d7bd46b408c1313
asratulhasannahid <mdnahid.prg@gmail.com>
2026-02-10 19:56:34 +0600

    dynamic routing with specification

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,7 +1,8 @@
-const page = () => {
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
   return (
     <div>
-      <h2 className="text-3xl">UserDetails</h2>
+      <h2 className="text-2xl">Showing details for user #{id}</h2>
     </div>
   );
 };
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -5,16 +5,16 @@ const page = () => {
     <div>
       <h2 className="text-5xl">Dashboard Users</h2>
       <ul className="mt-10">
-        <li>
+        <li className="text-2xl">
           <Link href="/dashboard/users/1">User - 1</Link>
         </li>
-        <li>
+        <li className="text-2xl">
           <Link href="/dashboard/users/2">User - 2</Link>
         </li>
-        <li>
+        <li className="text-2xl">
           <Link href="/dashboard/users/3">User - 3</Link>
         </li>
-        <li>
+        <li className="text-2xl">
           <Link href="/dashboard/users/4">User - 4</Link>
         </li>
       </ul>
