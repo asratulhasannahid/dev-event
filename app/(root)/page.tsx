e1992a5e979edefb1801ac1f0edb95c2b62ceb10
asratulhasannahid <mdnahid.prg@gmail.com>
2026-02-11 22:23:41 +0600

    I learn core concept and theory from this project

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,7 +6,7 @@ const Home = async () => {
   return (
     <main>
       <div className="text-5xl underline bg-red-500 text-center pt-10 pl-10 pr-10 pb-10">
-        Welcome to the world!
+        Welcome to the world!, I learn core concept and theory from this project
       </div>
       <Hallo></Hallo>
     </main>
